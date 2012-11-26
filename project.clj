dccdb0e028ae2f87bd6050779b000614ec56dbbf
Gabriel Horner <gabriel.horner@gmail.com>
2012-11-26 00:19:28 -0500

    upgrade deps per ibdknox/cljs-template#8

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject semtag-web "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://exampl.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [noir-cljs "0.3.0"]
-                 [jayq "0.1.0-alpha1"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [noir-cljs "0.3.4"]
+                 [jayq "0.3.0"]
                  [fetch "0.1.0-alpha2"]
-                 [crate "0.1.0-alpha3"]
-                 [noir "1.3.0-beta2"]]
+                 [crate "0.2.1"]
+                 [noir "1.3.0-beta8"]]
   :cljsbuild {:builds [{}]}
   :main ^{:skip-aot true} semtag-web.server)
