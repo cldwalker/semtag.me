f9da42ae823db92a366ac3a40ed260f754921aef
Gabriel Horner <gabriel.horner@gmail.com>
2013-08-20 23:13:40 -0400

    display results and search result
    * copy over crate partials
    * each search results in another table

--- pedestal-app/app/src/semtag_web/rendering.cljs
+++ pedestal-app/app/src/semtag_web/rendering.cljs
@@ -1,6 +1,7 @@
 (ns semtag-web.rendering
   (:require [domina :as dom]
             [semtag-web.rendering-util :as util]
+            [semtag-web.partials :as p]
             [io.pedestal.app.render.push :as render]
             [io.pedestal.app.messages :as msg]
             [io.pedestal.app.render.push.templates :as templates]
@@ -17,6 +18,13 @@
 (defn render-message [renderer [_ path _ new-value] transmitter]
   (dom/set-html! (dom/by-id "greeting") new-value))
 
+(defn render-search-results [_ [_ _ _ things] _]
+  (dom/insert-after! (dom/by-id "greeting")
+                     (p/generate-table "search_table" things
+                                       :fields [:type :name :url :desc :tags]
+                                       :row-partial p/tag-search-row
+                                       :caption (format "Total: %s" (count (map :url things))))))
+
 (defn url-search [{:keys [transform messages]}]
   (msg/fill transform messages {:value (.-value (dom/by-id "url_search_text"))}))
 
@@ -25,6 +33,7 @@
     into
     [[[:node-create [:app-model] render-page]
       [:node-destroy [:app-model] d/default-exit]
-      [:value [:app-model :greeting] render-message]]
+      [:value [:app-model :greeting] render-message]
+      [:value [:app-model :search-results] render-search-results]]
      (util/click [:app-model :search] "url_search_button" :fn url-search)
      (util/click [:app-model :set-greeting] "add_url_button" :fn url-search)]))
--- pedestal-app/app/src/semtag_web/simulated/services.cljs
+++ pedestal-app/app/src/semtag_web/simulated/services.cljs
@@ -4,6 +4,9 @@
 
 (defn services-fn [message input-queue]
   (.log js/console (str "Sending query to server: " message))
+  (p/put-message input-queue {msg/type :set-greeting
+                              msg/topic [:greeting]
+                              :value (format "Search results for '%s'" (:value message))})
   (p/put-message input-queue {msg/type :set-value
                               msg/topic [:search-results]
                               :value [{:url "http://funnyordie.com" :desc "tiz funny"}]}))
