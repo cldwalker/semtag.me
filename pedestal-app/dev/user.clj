a26574b49de552f78b56b898f9fc4b80cd9c3902
Gabriel Horner <gabriel.horner@gmail.com>
2013-08-24 14:30:34 -0400

    convenience fn for starting austin

--- pedestal-app/dev/user.clj
+++ pedestal-app/dev/user.clj
@@ -2,3 +2,13 @@
   []
   (require 'dev)
   (in-ns 'dev))
+
+(require 'cemerick.austin.repls)
+(defn repl
+  "Start Austin cljs-repl and saves repl environment to repl-env.
+  Connect a browser by calling this JS:
+    ;goog.require ('clojure.browser.repl');clojure.browser.repl.connect.call(null, ':repl-url');"
+  []
+  (defonce repl-env (cemerick.austin/repl-env))
+  (println (str "Started austin with repl-url " (:repl-url repl-env)))
+  (cemerick.austin.repls/cljs-repl repl-env))
