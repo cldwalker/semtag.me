e64fb5e9ac138baa1a14f18744b136b7ed990d16
Gabriel Horner <gabriel.horner@gmail.com>
2013-09-16 09:03:25 -0400

    search loads by url :)

--- pedestal-app/app/src/semtag_web/start.cljs
+++ pedestal-app/app/src/semtag_web/start.cljs
@@ -10,28 +10,37 @@
             [semtag-web.behavior :as behavior]
             [semtag-web.rendering :as rendering]
             [semtag-web.history :as history]
+            [clojure.string :as string]
             [goog.Uri]))
 
 (defn- put-message-on-page-load [app screen params]
-  ;; TODO
-  (if true
+  (if (re-find #"^search" (name screen))
     (p/put-message (:input app) (merge {msg/type :map-value msg/topic [:search]} params))
     ;; consider reuse with navbar-deltas
     (p/put-message (:input app) {msg/type :set-value msg/topic [:page] :value (name screen)})))
 
+;; use goog.Uri if it has an API for extracting param names - .getParameterValues doesn't cut it
+(defn- parse-params [url]
+  (when-let [params-string (re-find #"(?!.*\?).*" url)]
+    (-> params-string
+        (string/split #"\&")
+        (as-> pairs
+          (map #(let [[k v] (string/split % #"=")] [(keyword k) v]) pairs))
+        flatten
+        vec
+        (as-> vals (apply hash-map vals)))))
+
 (defn create-app [render-config]
-  (let [params {:query "ajax" :search-type "tagged"} ;; TODO
+  (let [params (parse-params window.location.hash)
         ;; For now we detect on hash. If I put a server to route all urls to this js app,
         ;; this could change to full urls.
         screen (or (route/url->screen (.-hash window.location) params)
                    (get-in behavior/example-app [:focus :default]))
-        _ (.log js/console "SCREEN" (pr-str screen))
-        behavior-with-new-default-focus (-> behavior/example-app
-                                            (assoc-in [:focus :default] screen)
-                                            (assoc-in [:focus screen]
-                                                      [[:app-model :search screen] [:app-model :home] [:app-model :navbar]]))
-        _ (.log js/console (pr-str (:focus behavior-with-new-default-focus)))
-        app (app/build behavior-with-new-default-focus)
+        behavior (cond-> behavior/example-app
+                   true (assoc-in [:focus :default] screen)
+                   (re-find #"^search" (name screen)) (assoc-in [:focus screen]
+                                                                [[:app-model :search screen] [:app-model :home] [:app-model :navbar]]))
+        app (app/build behavior)
         render-fn (push-render/renderer "content" render-config render/log-fn)
         app-model (render/consume-app-model app render-fn)]
     (app/begin app)
