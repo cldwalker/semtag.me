530d674fe73fcbe95d7bf80b3e8600c5437566c5
Gabriel Horner <gabriel.horner@gmail.com>
2013-09-27 08:49:52 -0400

    add tests for urls and move phantom driver to before/after hooks

--- pedestal-app/test/semtag_web/integration_test.clj
+++ pedestal-app/test/semtag_web/integration_test.clj
@@ -1,24 +1,47 @@
 (ns semtag-web.integration-test
   (:require [clojure.test :refer :all]
             [clj-webdriver.driver :refer [init-driver]]
+            [clj-webdriver.core :as core]
             [clj-webdriver.taxi :as taxi])
   (:import [org.openqa.selenium.phantomjs PhantomJSDriver]
            [org.openqa.selenium.remote DesiredCapabilities]))
 
-(deftest submit-test
-  (taxi/set-driver! (init-driver {:webdriver (PhantomJSDriver. (DesiredCapabilities. ))}))
-  (taxi/to "out/public/semtag-web-test.html")
-  (Thread/sleep 1000)
+(use-fixtures :each
+              (fn [f]
+                (taxi/set-driver! (init-driver {:webdriver (PhantomJSDriver. (DesiredCapabilities. ))}))
+                (taxi/to "out/public/semtag-web-test.html")
+                (Thread/sleep 1000)
+
+                (f)
+                (taxi/quit)))
+
+(defn click [text]
+  (core/click (taxi/find-element {:tag :a :text text}))
+  (Thread/sleep 500))
+
+(deftest ursl-get-updated-correctly
+  (is (.endsWith (taxi/current-url) "/semtag-web-test.html"))
+  (click "Tag Stats")
+  (is (.endsWith (taxi/current-url) "/semtag-web-test.html#/tag-stats"))
+
+  (click "Type Stats")
+  (is (.endsWith (taxi/current-url) "/semtag-web-test.html#/types"))
+
+  (click "All The Things")
+  (is (.endsWith (taxi/current-url) "/semtag-web-test.html#/all"))
+
+  (click "Home")
+  (is (.endsWith (taxi/current-url) "/semtag-web-test.html#/")))
+
+(deftest search-submit-works
   (taxi/input-text "#url_search_text" "feynman")
   (is (= "feynman" (taxi/attribute "#url_search_text" :value)))
+
   (taxi/click "#url_search_button")
   (Thread/sleep 1000)
-
-  (is (= "Search results for 'feynman'" (taxi/text "#search_title")))
-  (taxi/quit))
+  (is (= "Search results for 'feynman'" (taxi/text "#search_title"))))
 
 (comment
-  (clj-webdriver.core/click  (find-element  {:tag :a :text "Tag Stats"}))
   (taxi/take-screenshot :file "out.png")
   (println "LOGS: ")
   (doseq [log (taxi/execute-script "return logs;")] (println log)))
