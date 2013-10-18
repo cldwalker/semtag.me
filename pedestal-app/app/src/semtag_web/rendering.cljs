e37c88afd7ab870e7622a41749f63afdde42fee3
Gabriel Horner <gabriel.horner@gmail.com>
2013-10-17 21:59:35 -0400

    hard-coded modal window with spin.js and q keybinding to quit

--- pedestal-app/app/src/semtag_web/rendering.cljs
+++ pedestal-app/app/src/semtag_web/rendering.cljs
@@ -7,6 +7,8 @@
             [semtag-web.history :as history]
             [semtag-web.route :as route]
             [clojure.string :as string]
+            [goog.events.KeyCodes :as key-codes]
+            [goog.ui.KeyboardShortcutHandler :as shortcut]
             [io.pedestal.app.protocols :as prot]
             [io.pedestal.app.render.events :as events]
             [io.pedestal.app.render.push :as render]
@@ -209,14 +211,29 @@
 ;; seeemed saner than massaging emit deltas or coming up with
 ;; separate paths just to toggle a value.
 (defn render-modal-spinner [_ [_ _ _ new-value] _]
-  (when-not @gdialog
+  (when new-value
+      (.log js/console "key binding")
+    (let [shortcut-handler (goog.ui.KeyboardShortcutHandler. js/document)
+          show-triggered (fn [event]
+                           (.log js/console (str "Received: " (.-identifier event)))
+                           (.stop js/spinner)
+                           (set! (-> "spin_modal_overlay" dom/by-id .-style .-display)
+                                 "none"))]
+      (.registerShortcut shortcut-handler "q" "q")
+      (.listen goog.events
+               shortcut-handler
+               goog.ui.KeyboardShortcutHandler.EventType.SHORTCUT_TRIGGERED
+               show-triggered)))
+  #_(when-not @gdialog
     (reset! gdialog (new goog.ui.Dialog)))
-  (if new-value
-    (doto @gdialog
-      (.setContent new-value)
+  #_(if new-value
+    (dom/append!
+     (dom/by-id "body"))
+    #_(doto @gdialog
+      (.setContent "<img src='/large_spinner.gif'/>")
       (.setVisible true))
     ;; Add a little lag so it's not just a blink
-    (js/setTimeout (fn [] (.setVisible @gdialog false)) 200)))
+    (js/setTimeout (fn [] #_(.setVisible @gdialog false)) 200)))
 
 ;; TODO - undo for all :value's that render
 (defn render-config []
