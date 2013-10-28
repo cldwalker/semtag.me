0ebd482f3b2b49f642c2501415ed1cac336cda3b
Gabriel Horner <gabriel.horner@gmail.com>
2013-10-28 07:18:19 -0400

    update history to handle keyword being a cljs object
    Basically a keyword can't be a history token without getting
    a map in the popstate callback. After that had to change put-message
    and url-for fns that expect keywords.

--- pedestal-app/app/src/semtag_web/history.cljs
+++ pedestal-app/app/src/semtag_web/history.cljs
@@ -13,27 +13,30 @@
 
 (defn navigate [token]
   (when-let [d (get @input-queues token)]
-    (.log js/console "NAVIGATE" (pr-str token))
+    (.log js/console "NAVIGATE" (name token))
     (p/put-message d {msg/topic msg/app-model
                       msg/type :set-focus
-                      :name token})))
+                      :name (keyword token)})))
 
 (def supported? (and js/history (.-pushState js/history)))
 
 (defn navigated [d token]
   (when supported?
-    (.log js/console "NAVIGATED" (pr-str token))
-    (let [current-token (.-state js/history)]
-      (when (not= current-token token)
-        (if (nil? @last-page)
-          (.replaceState js/history token nil nil)
-          (.pushState js/history token nil (route/url-for token)))))
-    (reset! last-page token)
-    (swap! input-queues assoc token d)))
+    ;; must use strings as unique ids otherwise they come out funny
+    ;; in popstate callback
+    (let [token (name token)]
+      (.log js/console "NAVIGATED" token)
+      (let [current-token (.-state js/history)]
+        (when (not= current-token token)
+          (if (nil? @last-page)
+            (.replaceState js/history token nil nil)
+            (.pushState js/history token nil (route/url-for (keyword token))))))
+      (reset! last-page token)
+      (swap! input-queues assoc token d))))
 
 (if supported?
   (set! (.-onpopstate js/window) (fn [e]
-                                   (.log js/console "POP" (pr-str (.-state e)))
+                                   (.log js/console "POP" (.-state e))
                                    (navigate (.-state e))))
   (log/warn :in :semtag-web.history
             :message "HTML 5 History is not supported in this browser!"))
