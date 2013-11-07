deba5ea5adf446bbd1339513a4fc18de8501b4ac
Gabriel Horner <gabriel.horner@gmail.com>
2013-11-07 08:16:51 -0500

    add build option to generate site as READ_ONLY
    was better than messing with app-tools internals

--- pedestal-app/app/src/semtag_web/tasks.clj
+++ pedestal-app/app/src/semtag_web/tasks.clj
@@ -23,6 +23,16 @@
 (defn- ensure-relative-paths [html]
   (string/replace html #"/generated-js" "generated-js"))
 
+(def read-only-css
+  "<style>
+    #create_thing {
+      display: none;
+    }
+  </style>")
+
+(defn add-read-only-css [html]
+  (string/replace-first html #"(<link.*?semtag-web.css.*?>)" (str "$1" read-only-css)))
+
 (defn- modify-file
   [file transform-fn]
   (-> file
@@ -36,23 +46,30 @@
     config
     (assoc-in config [:aspects aspect :main] 'semtag_web.simulated.start)))
 
+(defn build-transform-fn [aspect]
+  (let [transform-js-fn (if (or (System/getenv "TEST") (= aspect :test))
+                          (comp ensure-relative-paths add-log-capturing) ensure-relative-paths)]
+    (if (System/getenv "READ_ONLY")
+      (comp add-read-only-css transform-js-fn) transform-js-fn)))
+
 (defn build-app
-  "Builds an aspect, :test if no argument is given. It also prepares it to be standalone html,
-  adds debugging for the test aspect and updates services/base-uri if $API_URI is set.
-  If $TEST is set, the current aspect prepares a builds as if it were a test aspect and forces
-  using simulated services."
+  "Builds an aspect, :test if no argument is given. It also prepares it to be standalone html and
+  adds debugging for the test aspect. Additional behavior can be toggled on with these env
+  variables:
+
+  * $API_URI: sets a new services/base-uri.
+  * $TEST: acts as if the current aspect were a test aspect by forcing simulated services.
+  * $READ_ONLY: hides any write-related UI e.g. the create_thing form."
   [& args]
   (let [aspect (keyword (or (first args) "test"))
-        {html-file :uri js-file :out-file} (-> dev/config vals first :aspects aspect)
-        transform-js-fn (if (or (System/getenv "TEST") (= aspect :test))
-                          (comp ensure-relative-paths add-log-capturing) ensure-relative-paths)]
+        {html-file :uri js-file :out-file} (-> dev/config vals first :aspects aspect)]
     (when-not html-file (throw (ex-info "This aspect does not exist!" {:aspect aspect})))
 
     (println (format "Building %s app..." aspect))
     (time (build/build! (-> dev/config vals first (modify-config aspect)) aspect))
 
     (println "Writing" (str "out/public" html-file))
-    (modify-file (str "out/public" html-file) transform-js-fn)
+    (modify-file (str "out/public" html-file) (build-transform-fn aspect))
 
     (when-let [uri (System/getenv "API_URI")]
       (println "Writing" (str "out/public/generated-js/" js-file))
